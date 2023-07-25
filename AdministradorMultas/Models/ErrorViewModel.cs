db71f8464614160aae1e12475d0883d82c697182
Santiago Cantos <irlidriel@gmail.com>
2023-07-25 16:53:45 -0300

    Agregar archivos de proyecto.

---
+++ AdministradorMultas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdministradorMultas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
