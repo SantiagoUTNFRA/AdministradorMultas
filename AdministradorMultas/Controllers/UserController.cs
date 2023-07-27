55365d365e1ddfa5c834d8595a67ba0a6c0207da
Santiago Cantos <santicantos96@gmail.com>
2023-07-27 00:32:57 -0300

    todo

---
+++ AdministradorMultas/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using AdministradorMultas.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AdministradorMultas.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Usuarios()
+        {
+            return View();
+        }
+
+    }
+}
--- AdministradorMultas/Program.cs
+++ AdministradorMultas/Program.cs
@@ -18,6 +18,7 @@ namespace AdministradorMultas
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
